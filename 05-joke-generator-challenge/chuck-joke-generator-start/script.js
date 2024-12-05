23a7b0818e7e947ca30f0961c59c05b1c71db755
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-06 00:15:50 +0800

    feat:创建generateJoke函数

--- 05-joke-generator-challenge/chuck-joke-generator-start/script.js
+++ 05-joke-generator-challenge/chuck-joke-generator-start/script.js
@@ -1,4 +1,8 @@
 const jokeEl = document.getElementById("joke")
 const jokeBtn = document.getElementById("joke-btn")
 
+const generateJoke = () => {
+  console.log("123")
+}
+
 jokeBtn.addEventListener("click", generateJoke)
