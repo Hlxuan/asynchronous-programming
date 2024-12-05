fa73b6a093ec9ace5a93d4159465effdd1e6d2f6
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-05 22:50:52 +0800

    feat:为“停止”按钮添加事件监听器并调用clearInterval

--- 02-setinterval-clearinterval/script.js
+++ 02-setinterval-clearinterval/script.js
@@ -3,3 +3,8 @@ let intervalID = setInterval(myCallback, 1000)
 function myCallback() {
   console.log(Date.now())
 }
+
+function stopChange() {
+  clearInterval(intervalID)
+}
+document.getElementById("stop").addEventListener("click", stopChange)
