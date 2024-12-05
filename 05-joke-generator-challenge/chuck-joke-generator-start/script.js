cf8f85835911874b3754adbf2882c94d6e71fd2f
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-06 00:19:24 +0800

    feat:请求API数据显示一个笑话

--- 05-joke-generator-challenge/chuck-joke-generator-start/script.js
+++ 05-joke-generator-challenge/chuck-joke-generator-start/script.js
@@ -2,7 +2,21 @@ const jokeEl = document.getElementById("joke")
 const jokeBtn = document.getElementById("joke-btn")
 
 const generateJoke = () => {
-  console.log("123")
+  const xhr = new XMLHttpRequest()
+  xhr.open("GET", "https://api.chucknorris.io/jokes/random", true)
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        const joke = JSON.parse(this.responseText).value
+        jokeEl.innerHTML = joke
+      } else {
+        jokeEl.innerHTML = "出现错误，不好笑。"
+      }
+    }
+  }
+
+  xhr.send()
 }
 
 jokeBtn.addEventListener("click", generateJoke)
