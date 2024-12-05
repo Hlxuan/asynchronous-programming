36d06cc094dabbd5a76ce40425420a07f1aa48aa
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-05 23:36:58 +0800

    feat:自定义回调函数并获取数据

--- 03-callbacks/script.js
+++ 03-callbacks/script.js
@@ -1,6 +1,23 @@
-function toggle(e) {
-  console.log('callback ran');
-  e.target.classList.toggle('danger');
+// function toggle(e) {
+//   console.log("callback ran")
+//   e.target.classList.toggle("danger")
+// }
+
+// document.querySelector("button").addEventListener("click", toggle)
+
+const posts = [
+  { title: "帖子一", body: "这是帖子一" },
+  { title: "帖子二", body: "这是帖子二" },
+]
+
+function getPosts() {
+  setTimeout(() => {
+    posts.forEach((post) => {
+      const div = document.createElement("div")
+      div.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`
+      document.querySelector("#posts").appendChild(div)
+    })
+  }, 1000)
 }
 
-document.querySelector('button').addEventListener('click', toggle);
+getPosts()
