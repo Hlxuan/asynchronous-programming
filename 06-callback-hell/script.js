4f524ff0f9f8dd068d12843d5e47817348eee5a4
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-06 00:30:12 +0800

    feat:设置事件处理器

--- 06-callback-hell/script.js
+++ 06-callback-hell/script.js
@@ -1,4 +1,10 @@
 function getData(endpoint) {
   const XHR = new XMLHttpRequest()
   XHR.open("GET", endpoint)
+
+  XHR.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      console.log(JSON.parse(this.responseText))
+    }
+  }
 }
