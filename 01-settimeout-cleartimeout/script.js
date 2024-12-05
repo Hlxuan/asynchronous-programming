9d2e463b01143737ae234450b8b99dcdd7ec7f24
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-05 22:25:11 +0800

    feat:使用命名函数

--- 01-settimeout-cleartimeout/script.js
+++ 01-settimeout-cleartimeout/script.js
@@ -3,3 +3,9 @@ setTimeout(function () {
 }, 2000)
 
 console.log("来自全局作用域的输出")
+
+function changeText() {
+  const h1 = document.querySelector("h1")
+  h1.textContent = "来自回调的输出"
+}
+setTimeout(changeText, 2000)
