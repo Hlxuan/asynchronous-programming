63e85817076fc65b5916b66c482abbf7d6e46912
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-06 00:38:05 +0800

    update:使用回调确保顺序

--- 06-callback-hell/script.js
+++ 06-callback-hell/script.js
@@ -1,18 +1,22 @@
-function getData(endpoint) {
+function getData(endpoint, cb) {
   const XHR = new XMLHttpRequest()
   XHR.open("GET", endpoint)
-
   XHR.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      console.log(JSON.parse(this.responseText))
+      cb(JSON.parse(this.responseText))
     }
   }
-
   setTimeout(() => {
     XHR.send()
   }, Math.floor(Math.random() * 3000) + 1000)
 }
 
-getData("./movies.json")
-getData("./actors.json")
-getData("./directors.json")
+getData("./movies.json", (moviesData) => {
+  console.log(moviesData)
+  getData("./actors.json", (actorsData) => {
+    console.log(actorsData)
+    getData("./directors.json", (directorsData) => {
+      console.log(directorsData)
+    })
+  })
+})
