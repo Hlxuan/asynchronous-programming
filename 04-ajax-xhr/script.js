20c91eb3d3be33be1ce9c0caf126d8f1f78368a3
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-06 00:00:59 +0800

    update:从API获取数据

--- 04-ajax-xhr/script.js
+++ 04-ajax-xhr/script.js
@@ -1,15 +1,17 @@
 var xhr = new XMLHttpRequest()
 
-xhr.open("GET", "./movies.json")
+// xhr.open("GET", "./movies.json")
+
+xhr.open("GET", "https://api.github.com/users/hlxuan/repos")
 
 xhr.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
     // console.log(JSON.parse(this.responseText))
     const data = JSON.parse(this.responseText)
 
-    data.forEach((movie) => {
+    data.forEach((repo) => {
       const li = document.createElement("li")
-      li.innerHTML = `<strong>${movie.title}</strong> - ${movie.year}`
+      li.innerHTML = `<strong>${repo.name}</strong> - ${repo.created_at}`
       document.querySelector("#results").appendChild(li)
     })
   }
