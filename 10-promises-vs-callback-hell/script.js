4aaa3c7f7eec27b32555bd286887ae0645f3fd95
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-06 23:22:55 +0800

    update:链式获取其他数据

--- 10-promises-vs-callback-hell/script.js
+++ 10-promises-vs-callback-hell/script.js
@@ -21,6 +21,18 @@ function getData(endpoint) {
   })
 }
 
-getData("./movies.json").then((movies) => {
-  console.log(movies)
-})
+getData("./movies.json")
+  .then((movies) => {
+    console.log(movies)
+    return getData("./actors.json")
+  })
+  .then((actors) => {
+    console.log(actors)
+    return getData("./directors.json")
+  })
+  .then((directors) => {
+    console.log(directors)
+  })
+  .catch((error) => {
+    console.log("出现了问题")
+  })
