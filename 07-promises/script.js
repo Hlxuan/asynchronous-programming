c7d51425bbb4564a7d8b01ec1377f3837d6ae7ce
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-06 21:54:41 +0800

    feat:直接链式调用Promise而非将其赋值给变量

--- 07-promises/script.js
+++ 07-promises/script.js
@@ -10,3 +10,12 @@ promise.then(() => {
 })
 
 console.log("来自全局作用域的输出")
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("异步任务2完成")
+    resolve()
+  }, 1000)
+}).then(() => {
+  console.log("Promise 2已消耗")
+})
